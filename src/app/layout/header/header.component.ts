2cb8b9da9b29e3225696fc077b9d075d8465867e
Shivraj Hulbatte <shivrajhulbatte@gmail.com>
2021-03-29 16:18:49 +0530

    adding user suggestions

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-header',
@@ -8,9 +10,15 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  allUsers: User[] = [];
+  constructor(private userService: UserService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.allUsers = this.userService.getUsers().reverse();
+    this.userService.usersDataChanged.subscribe((newUsersData:User[])=>{
+      console.log('>>newusersdata',newUsersData);
+      this.allUsers = newUsersData.reverse();
+    });    
   }
 
   onAddUserClick(){
