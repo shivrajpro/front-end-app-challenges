24c46aea8540072a71f32939f511ecf63c58a455
Shivraj Hulbatte <shivrajhulbatte@gmail.com>
2021-04-23 12:51:05 +0530

    creating books-list cmp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BooksListComponent } from './books/books-list/books-list.component';
+import { BooksComponent } from './books/books/books.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'books',
+    pathMatch: 'full',
+  },
+  {
+    path: 'books',
+    component: BooksComponent,
+    children: [
+      {
+        path: ":genre",
+        component: BooksListComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/books/books/books.component.ts
+++ src/app/books/books/books.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.scss']
 })
-export class AppComponent {
+export class BooksComponent implements OnInit {
 
   constructor(private router: Router){
 
+  }
+
+  ngOnInit(): void {
   }
 
   onGenreCardClick(evt){
